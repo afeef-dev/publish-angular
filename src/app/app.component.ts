7e4f1778f8a5fe3cc3e473d50ed07a195699e6a9
afeef-dev <mail.afeef.dev@gmail.com>
2025-01-26 18:06:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  sub!: Subscription;
   title = 'publish-app';
+  time = Date.now().toString();
+
+  ngOnInit(): void {
+    this.sub = interval(1000).subscribe((d: number) => {
+      this.time = Date.now().toString();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
